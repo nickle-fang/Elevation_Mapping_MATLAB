45495e6a397600abf9555e4a0d01417771e1d682
nickle-fang <fangxz1998@gmail.com>
2021-06-26 15:02:04 +0800

    fix xy error

--- visualize_mesh.m
+++ visualize_mesh.m
@@ -1,12 +1,12 @@
 function [] = visualize_mesh(x_mesh_vector,y_mesh_vector,grid_height, covariance)
 %VISUALIZE_MESH Summary of this function goes here
 %   Detailed explanation goes here
-[X, Y] = meshgrid(x_mesh_vector, y_mesh_vector);
+[X, Y] = meshgrid(y_mesh_vector, x_mesh_vector);
 Z = grid_height;
 covariance = abs(covariance) .* (abs(covariance) < 0.5);
 surf(X, Y, Z, covariance);
 axis equal;
-xlabel('x');
-ylabel('y');
+xlabel('y');
+ylabel('x');
 end
 
